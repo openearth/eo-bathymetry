ce1a4c245f8ba5f7fd658c9a7d9d7faa521065e0
Ruben.White <rubenwhitenl@gmail.com>
2024-11-27 18:29:01 +0100

    filtering boxes

--- notebooks/cmems-bathymetry/gebco_geojsons_to_parquet_RW.py
+++ notebooks/cmems-bathymetry/gebco_geojsons_to_parquet_RW.py
@@ -7,20 +7,28 @@ from time import time
 
 # %%
 # Select year
-year = 2024
+year = '2024_latminus2'
 
 # File paths in
-if year == 2023:
+if year == '2023':
     dir_path_geojsons = r'p:\11209821-cmems-global-sdb\00_miscellaneous\Feasibility_maps\2023\Buffered\AOI_results\*\gebco_2023_*_result.geojson'
-elif year == 2024:
+elif year == '2024':
     dir_path_geojsons = r'p:\11209821-cmems-global-sdb\00_miscellaneous\Feasibility_maps\2024\AOI_results_2024\gebco_2024_*_result.geojson'
+elif year == '2024_latminus1':
+    dir_path_geojsons = r'p:\11209821-cmems-global-sdb\00_miscellaneous\Feasibility_maps\2024\AOI_results_2024_latminus1\gebco_2024_*_result.geojson'
+elif year == '2024_latminus2':
+    dir_path_geojsons = r'p:\11209821-cmems-global-sdb\00_miscellaneous\Feasibility_maps\2024\AOI_results_2024_latminus2\gebco_2024_*_result.geojson'
 file_path_geojsons = glob.glob(dir_path_geojsons)
 
 # File paths out
-if year == 2023:
+if year == '2023':
     file_path_merge_parquet = r'p:\11209821-cmems-global-sdb\00_miscellaneous\Feasibility_maps\Buffered\AOI_results\gebco_2023_merge_result.parquet'
-elif year == 2024:
+elif year == '2024':
     file_path_merge_parquet = r'p:\11209821-cmems-global-sdb\00_miscellaneous\Feasibility_maps\2024\gebco_2024_merge_result.parquet'
+elif year == '2024_latminus1':
+    file_path_merge_parquet = r'p:\11209821-cmems-global-sdb\00_miscellaneous\Feasibility_maps\2024\gebco_2024_latminus1_merge_result.parquet'
+elif year == '2024_latminus2':
+    file_path_merge_parquet = r'p:\11209821-cmems-global-sdb\00_miscellaneous\Feasibility_maps\2024\gebco_2024_latminus2_merge_result.parquet'
 
 # %%
 # Load geodataframes
